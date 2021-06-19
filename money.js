ce5b9685492b98bf6e3156c50f23e63e5a3f3c88
KhanhLVH <919300+lvhkhanh@users.noreply.github.com>
2021-06-19 22:12:15 +0700

    Create money.js

---
+++ money.js
@@ -0,0 +1,9 @@
+function spend(total, amount) {
+  // TODO: check amount should be less than total
+  return total > amount;
+}
+
+function add(total, amount) {
+  // TODO: add amount savings to total
+  total += amount;
+}
