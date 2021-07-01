123da63141957ed7eb3bceb3dd0712e594a13a43
KhanhLVH <919300+lvhkhanh@users.noreply.github.com>
2021-07-01 10:35:51 +0700

    Update data_science.js

--- data_science.js
+++ data_science.js
@@ -1,3 +1,7 @@
 function describe(data){
 //   TODO: columns present fields, rows for count, mean, std, min, 25%, 50%, 75%, max
 }
+
+function percentile(data, percent){
+  // TODO: find the value in data that percent% of data is lower than
+}
